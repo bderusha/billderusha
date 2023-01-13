35787f3ec01a378b22b99b9a53cd471fe9523d7f
Bill DeRusha <444835+bderusha@users.noreply.github.com>
2023-01-13 17:38:14 -0500

    initialize garden

---
+++ _next/static/7wvoXMsDM36hlLIJBlvhI/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/_all":["static/chunks/pages/_all-9494210ce27082d5.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],"/[[...slug]]":["static/chunks/pages/[[...slug]]-2f88b743ceda4ac6.js"],sortedPages:["/_all","/_app","/_error","/[[...slug]]"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
