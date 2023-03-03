cb8792e451c34b65641ae78cd10a1bb946cc1add
bderusha-personal <bill.derusha@gmail.com>
2023-03-02 21:21:13 -0500

    splendor page

--- docs/obs.html/static/obsidian_core.js
+++ docs/obs.html/static/obsidian_core.js
@@ -10,7 +10,7 @@ var CONFIGURED_HTML_URL_PREFIX = "";
 var RELATIVE_PATHS = 0;
 var documentation_mode = 1;
 var tab_mode = !no_tab_mode;
-var gzip_hash = '285027281048165212548761795986298942831'                       // used to check whether the localStorage data is stale
+var gzip_hash = '223243208155315796499002272566472767559'                       // used to check whether the localStorage data is stale
 
 // global cache
 var fn_cache_ls_available = null;
